b54040536c7e13597e708d7dd1c42988d5320a94
Anjey Tsibylskij <130153594+atldays@users.noreply.github.com>
2026-01-17 23:04:33 +0200

    fix(release): specify npm registry in release-it config

--- .release-it.cjs
+++ .release-it.cjs
@@ -142,6 +142,7 @@ module.exports = () => {
             skipChecks: true,
             provenance: true,
             access: "public",
+            registry: "https://registry.npmjs.org/",
             versionArgs: ["--no-git-tag-version"],
         },
 
